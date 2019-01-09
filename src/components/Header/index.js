86d5a91065b68bbf0893a0b984ad7e867f6cd684
juice-tn <jihu102938@gmail.com>
2019-01-09 03:31:01 -0800

    style header

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Toronto Waste Lookup</h1>
+    </div>
+  )
+};
+
+export default Header;
---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+class Search extends React.Component {
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default Search;
